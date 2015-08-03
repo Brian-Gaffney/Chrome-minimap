ea86a37d94f52b8c57e61bb5b4775072c6141732
Brian Gaffney <brian@deb.ug>
2015-08-03 22:32:50 +1000

    Many changes and improvements.

--- minimap.js
+++ minimap.js
@@ -6,40 +6,101 @@ var html = document.documentElement;
 //Page height
 var pageHeight = Math.max(body.scrollHeight,body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
-
 var elementId = 'minimap-extension-element';
 
 //Create element to contain the minimap canvas
 var minimapElement = document.createElement('div');
 minimapElement.setAttribute('id', elementId);
 
-//Create overlay window element
+//Create overlay viewport element
 var viewportElement = document.createElement('div');
-viewportElement.setAttribute('id', elementId + '-window');
+viewportElement.setAttribute('id', elementId + '-viewport');
 
 minimapElement.appendChild(viewportElement);
 
 body.appendChild(minimapElement);
 
-html2canvas(body, {
-	width: 100,
-	height: 100,
-	onrendered: function(canvas) {
-		minimapElement.appendChild(canvas);
+var firstRun = true;
 
-		var minimapHeight = minimapElement.offsetHeight;
+var takeScreenshot = _.debounce(function(){
+	var canvas = minimapElement.querySelector('canvas');
 
-		//Set window element height to represent scroll window height relative to document
-		var percentageOfPageVisible = (window.innerHeight / pageHeight);
-		viewportElement.style.height = (percentageOfPageVisible * minimapHeight) + 'px';
+	if(canvas) {
+		canvas.remove();
+	}
 
-		//Update inner map each second
-		window.setInterval(function() {
-			var scrollPercentage = window.scrollY / window.innerHeight;
+	//Take the screenshot
+	html2canvas(body, {
+		width: 100,
+		height: 100,
+		onrendered: function(canvas) {
+			minimapElement.appendChild(canvas);
 
-			var top = scrollPercentage * minimapHeight;
+			if(firstRun) {
+				firstRun = false;
+				minimapElement.className = 'show';
+			}
 
-			viewportElement.style.top = top + 'px';
-		}, 1000);
+			var minimapHeight = minimapElement.offsetHeight;
+
+			//Set viewport element height to represent scroll window height relative to document
+			var percentageOfPageVisible = (window.innerHeight / pageHeight);
+			viewportElement.style.height = (percentageOfPageVisible * minimapHeight) + 'px';
+
+			updateViewportPosition();
+		}
+	});
+}, 250);
+
+takeScreenshot();
+
+function updateScreenshot() {
+	console.log('updateScreenshot()');
+}
+
+function updateViewportPosition() {
+	console.log('updateViewportPosition()');
+
+	var scrollPercentage = window.scrollY / window.innerHeight;
+
+	var top = scrollPercentage * minimapElement.offsetHeight;
+
+	viewportElement.style.top = top + 'px';
+}
+
+function toggleVisibility() {
+	minimapElement.className = minimapElement.className === 'show' ? '' : 'show';
+}
+
+//Listen for scroll events
+window.onscroll = updateViewportPosition;
+
+//Listen for messages from background.js
+chrome.runtime.onMessage.addListener(function(message, sender, callback) {
+	switch(message) {
+		case 'iconClicked':
+			toggleVisibility();
+			break;
+
+		default:
+			console.warn('Unknown message recieved', message);
 	}
 });
+
+//Listen for DOM changes
+var DOMSubtreeModified = _.debounce(function(ev) {
+
+
+	if(ev.target.id === elementId) {
+		console.log('#####');
+		return;
+	}
+	
+	console.log('DOMSubtreeModified');
+	// updateScreenshot();
+
+	takeScreenshot();
+
+	updateViewportPosition();
+}, 250);
+document.addEventListener('DOMSubtreeModified', DOMSubtreeModified);
---
+++ readme.md
@@ -0,0 +1,7 @@
+*Todo
+
+* Hide/show on icon click
+* Click and drag viewport to scroll
+* Bind to DOM changes and rerender canvas
+* Minimap should scroll when it doesn't fit on the page
+* Click non-viewport area to scroll
